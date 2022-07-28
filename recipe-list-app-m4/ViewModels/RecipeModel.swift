5a52a249700f9ff0f29b271a39a990183c70c629
Sarah Casagrande <sarah.e.casagrande@gmail.com>
2022-07-28 13:51:35 -0400

    Module 4 Lesson 12

--- recipe-list-app-m4/ViewModels/RecipeModel.swift
+++ recipe-list-app-m4/ViewModels/RecipeModel.swift
@@ -15,4 +15,63 @@ class RecipeModel: ObservableObject {
         // Create an instance of data service and get data
         self.recipes = DataService.getLocalData()
     }
+    
+    static func getPortion(ingredient:Ingredient, recipeServings:Int, targetServings: Int) -> String {
+        
+        var portion = ""
+        var numerator = ingredient.num ?? 1
+        var denominator = ingredient.denom ?? 1
+        var wholePortions = 0
+        
+        if ingredient.num != nil {
+            // Get a single serving size
+            denominator *= recipeServings
+            
+            // Get target portion by multiplying numerator by servings
+            numerator *= targetServings
+            
+            // Reduce fraction
+            let divisor = Rational.greatestCommonDivisor(numerator, denominator)
+            numerator /= divisor
+            denominator /= divisor
+            
+            // Get the whole portions
+            if numerator >= denominator {
+                wholePortions = numerator / denominator
+                numerator = numerator % denominator
+                portion += String(wholePortions)
+            }
+            
+            // Express remainder as fraction
+            
+            if numerator > 0 {
+                portion += wholePortions > 0 ? " " : ""
+                portion += "\(numerator)/\(denominator)"
+            }
+            
+        }
+    
+        if var unit = ingredient.unit {
+            
+            if wholePortions > 1 {
+                
+                if unit.suffix(2) == "ch" {
+                    unit += "es"
+                } else if unit.suffix(1) == "f" {
+                    unit = String(unit.dropLast())
+                    unit += "ves"
+                } else {
+                    unit += "s"
+                }
+                
+            }
+            
+            portion += ingredient.num == nil && ingredient.denom == nil ? "" : " "
+        
+            return portion + unit
+        }
+        
+        return portion
+    }
+    
 }
